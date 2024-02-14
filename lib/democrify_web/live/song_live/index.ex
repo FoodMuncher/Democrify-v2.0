0d460167bddc7fcfe2c6ebfe4b3c497e439ba796
Joe Edward <joeedward@github.com>
2024-02-14 21:25:57 +0000

    Removed changeset

--- lib/democrify_web/live/song_live/index.ex
+++ lib/democrify_web/live/song_live/index.ex
@@ -6,7 +6,7 @@ defmodule DemocrifyWeb.SongLive.Index do
   alias Democrify.Session.Song
   alias Democrify.Spotify.{Search, Tracks, Track, Profile}
 
-  @send_after 750
+  @check_query_wait 750 # 3/4 of a second.
 
   # =================================
   # Live View Callbacks
@@ -62,28 +62,21 @@ defmodule DemocrifyWeb.SongLive.Index do
 
     {:noreply, assign(socket, :session, songs)}
   end
-  def handle_event("load_suggestions", %{"song" => song_params}, socket) do
-    query = song_params
-    |> Map.get("query", "")
-    |> String.trim()
+  def handle_event("add_song_query", %{"query" => query}, socket) do
+    query = String.trim(query)
 
-    Process.send_after(self(), {:check_query, query}, @send_after)
+    Process.send_after(self(), {:check_query, query}, @check_query_wait)
 
-    socket = assign(socket, :query, query)
-
-      {:noreply, socket
-        |> assign(:query,           query)
-        |> assign(:suggested_songs, loading?(query))}
+    {:noreply, socket
+    |> assign(:query,           query)
+    |> assign(:suggested_songs, loading?(query))}
 
   end
-  def handle_event("save", %{"song" => song_params}, socket) do
-    case Map.fetch(song_params, "track_id") do
-      {:ok, track_id} ->
-        {:noreply, save_song(socket, socket.assigns.action, track_id)}
-
-      :error ->
-        {:noreply, put_flash(socket, :error, "No song selectd")}
-    end
+  def handle_event("save", %{"track_id" => track_id}, socket) do
+    {:noreply, save_song(socket, socket.assigns.action, track_id)}
+  end
+  def handle_event("save", _params, socket) do
+    {:noreply, put_flash(socket, :error, "No song selectd")}
   end
 
   @impl true
@@ -92,15 +85,15 @@ defmodule DemocrifyWeb.SongLive.Index do
   end
   def handle_info({:updated_spotify_data, spotify_data}, socket) do
     Logger.info("Index Live View #{socket.assigns.session_id} received new spotify_data")
+    # TODO: Add spotify_data to the session, if you refresh te page after the token has
+    #       refreshed it'll pull old token from the session.
     {:noreply, assign(socket, :spotify_data, spotify_data)}
   end
-  def handle_info({:check_query, query}, socket) do
-    if socket.assigns.query == query do
-      suggested_songs = get_suggested_songs(query, socket.assigns.spotify_data)
-      {:noreply, assign(socket, :suggested_songs, suggested_songs)}
-    else
-      {:noreply, socket}
-    end
+  def handle_info({:check_query, query}, socket = %{assigns: %{query: query}}) do
+    {:noreply, assign(socket, :suggested_songs, get_suggested_songs(query, socket.assigns.spotify_data))}
+  end
+  def handle_info({:check_query, _old_query}, socket) do
+    {:noreply, socket}
   end
 
   # =================================
@@ -108,20 +101,14 @@ defmodule DemocrifyWeb.SongLive.Index do
   # =================================
 
   defp apply_action(socket, :edit, %{"id" => song_id}) do
-    song = Session.get_song!(song_id, socket.assigns.session_id)
-
     socket
     |> assign(:page_title, "Edit Song")
-    |> assign(:song,       song)
-    |> assign(:changeset,  Session.change_song(song))
+    |> assign(:song,       Session.get_song!(song_id, socket.assigns.session_id))
   end
   defp apply_action(socket, :new, _params) do
-    song = %Song{}
-
     socket
     |> assign(:page_title, "Add Song")
-    |> assign(:song,       song)
-    |> assign(:changeset,  Session.change_song(song))
+    |> assign(:song,       %Song{})
   end
   defp apply_action(socket, :index, _params) do
     socket
@@ -169,9 +156,7 @@ defmodule DemocrifyWeb.SongLive.Index do
     |> push_redirect(to: ~p"/session")
   end
 
-  defp convert_tracks([]) do
-    []
-  end
+  defp convert_tracks([]), do: []
   defp convert_tracks([track = %Track{} | tracks]) do
     [{"#{track.name} - #{Song.artists(track.artists)}", track.id} | convert_tracks(tracks)]
   end
