cab28d0dcd241037ae3c992fd41269591b9baf11
Joe Edward <joeedward@github.com>
2024-02-14 21:11:06 +0000

    Search optimisations

--- lib/democrify_web/live/song_live/form_component.ex
+++ lib/democrify_web/live/song_live/form_component.ex
@@ -1,92 +1,39 @@
 defmodule DemocrifyWeb.SongLive.FormComponent do
   use DemocrifyWeb, :live_component
 
-  alias Democrify.{Session, Spotify}
-  alias Democrify.Session.Song
-  alias Democrify.Spotify.{Search, Tracks, Track}
+  alias Democrify.Session
+  require Logger
 
   # =================================
   # Live View Callbacks
   # =================================
 
   @impl true
-  def mount(socket) do
-    {:ok, assign(socket, :suggested_songs, nil)}
-  end
-
-  @impl true
   def update(%{song: song} = assigns, socket) do
+    Logger.error("Change: #{inspect Session.change_song(song)}")
+
     {:ok, socket
      |> assign(assigns)
      |> assign(:changeset, Session.change_song(song))}
   end
 
-  @impl true
-  def handle_event("load_suggestions", %{"song" => song_params}, socket) do
-    query = song_params
-    |> Map.get("query", "")
-    |> String.trim()
-
-    suggested_songs =
-      unless query == "" do
-        case Spotify.search_tracks(query, socket.assigns.spotify_data) do
-          {:ok, %Search{tracks: %Tracks{items: tracks}}} when tracks != [] ->
-            convert_tracks(tracks)
-
-          _response ->
-            nil
-        end
-      end
-
-    changeset =
-      socket.assigns.song
-      |> Session.change_song(song_params)
-      |> Map.put(:action, :validate)
-
-    {:noreply, socket
-      |> assign(:changeset, changeset)
-      |> assign(:suggested_songs, suggested_songs)}
-  end
-  def handle_event("save", %{"song" => song_params}, socket) do
-    save_song(socket, socket.assigns.action, song_params)
-  end
-
   # =================================
-  # Internal Functions
+  # Live View Components
   # =================================
 
-  defp save_song(socket, :new, song_params) do
-    Session.create_song(
-      song_params["track_id"],
-      socket.assigns.session_id,
-      socket.assigns.spotify_data,
-      socket.assigns.username,
-      socket.assigns.user_id
-    )
-
-    {:noreply,
-     socket
-     |> put_flash(:info, "Song created successfully")
-     |> push_redirect(to: socket.assigns.return_to)}
-  end
-  defp save_song(socket, :edit, song_params) do
-    Session.update_song(
-      song_params["track_id"],
-      socket.assigns.session_id,
-      socket.assigns.spotify_data,
-      socket.assigns.song
-    )
-
-    {:noreply,
-     socket
-     |> put_flash(:info, "Song updated successfully")
-     |> push_redirect(to: socket.assigns.return_to)}
-  end
-
-  defp convert_tracks([]) do
-    []
-  end
-  defp convert_tracks([track = %Track{} | tracks]) do
-    [{"#{track.name} - #{Song.artists(track.artists)}", track.id} | convert_tracks(tracks)]
+  defp loading_svg(assigns) do
+    ~H"""
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 78">
+        <circle fill="#1ed760" stroke="#1ed760" stroke-width="2" r="15" cx="120" cy="39">
+          <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"/>
+        </circle>
+        <circle fill="#1ed760" stroke="#1ed760" stroke-width="2" r="15" cx="200" cy="39">
+          <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"/>
+        </circle>
+        <circle fill="#1ed760" stroke="#1ed760" stroke-width="2" r="15" cx="280" cy="39">
+          <animate attributeName="opacity" calcMode="spline" dur="2" values="1;0;1;" keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"/>
+        </circle>
+      </svg>
+    """
   end
 end
--- lib/democrify_web/live/song_live/index.ex
+++ lib/democrify_web/live/song_live/index.ex
@@ -2,10 +2,11 @@ defmodule DemocrifyWeb.SongLive.Index do
   require Logger
   use DemocrifyWeb, :live_view
 
-  alias Democrify.Session
-  alias Democrify.Spotify
+  alias Democrify.{Session, Spotify}
   alias Democrify.Session.Song
-  alias Democrify.Spotify.Profile
+  alias Democrify.Spotify.{Search, Tracks, Track, Profile}
+
+  @send_after 750
 
   # =================================
   # Live View Callbacks
@@ -18,9 +19,12 @@ defmodule DemocrifyWeb.SongLive.Index do
     if Session.exists?(session_id) do
       spotify_data = session["spotify_data"]
 
-      if connected?(socket) do
+      socket = if connected?(socket) do
         Session.subscribe(session_id)
         Spotify.subscribe(spotify_data)
+        assign(socket, :session, Session.list_session(session_id))
+      else
+        assign(socket, :session, [])
       end
 
       # TODO: Safer handling here....
@@ -28,11 +32,10 @@ defmodule DemocrifyWeb.SongLive.Index do
 
       {:ok,
        socket
-       |> assign(:user_id,      profile.id)
-       |> assign(:username,     profile.display_name)
-       |> assign(:session,      Session.list_session(session_id))
-       |> assign(:session_id,   session_id)
-       |> assign(:spotify_data, spotify_data)}
+       |> assign(:user_id,        profile.id)
+       |> assign(:username,       profile.display_name)
+       |> assign(:session_id,     session_id)
+       |> assign(:spotify_data,   spotify_data)}
     else
       {:ok, redirect(socket, to: ~p"/")}
     end
@@ -40,10 +43,12 @@ defmodule DemocrifyWeb.SongLive.Index do
 
   @impl true
   def handle_params(params, _url, socket) do
-    {:noreply,
-     socket
-     |> assign(:session_id, socket.assigns.session_id)
-     |> apply_action(socket.assigns.live_action, params)}
+    {:noreply, socket
+      |> assign(:session_id,      socket.assigns.session_id)
+      |> assign(:action,           socket.assigns.live_action)
+      |> assign(:query,           "")
+      |> assign(:suggested_songs, nil)
+      |> apply_action(socket.assigns.live_action, params)}
   end
 
   @impl true
@@ -57,6 +62,29 @@ defmodule DemocrifyWeb.SongLive.Index do
 
     {:noreply, assign(socket, :session, songs)}
   end
+  def handle_event("load_suggestions", %{"song" => song_params}, socket) do
+    query = song_params
+    |> Map.get("query", "")
+    |> String.trim()
+
+    Process.send_after(self(), {:check_query, query}, @send_after)
+
+    socket = assign(socket, :query, query)
+
+      {:noreply, socket
+        |> assign(:query,           query)
+        |> assign(:suggested_songs, loading?(query))}
+
+  end
+  def handle_event("save", %{"song" => song_params}, socket) do
+    case Map.fetch(song_params, "track_id") do
+      {:ok, track_id} ->
+        {:noreply, save_song(socket, socket.assigns.action, track_id)}
+
+      :error ->
+        {:noreply, put_flash(socket, :error, "No song selectd")}
+    end
+  end
 
   @impl true
   def handle_info({:songs_changed, songs}, socket) do
@@ -66,24 +94,85 @@ defmodule DemocrifyWeb.SongLive.Index do
     Logger.info("Index Live View #{socket.assigns.session_id} received new spotify_data")
     {:noreply, assign(socket, :spotify_data, spotify_data)}
   end
+  def handle_info({:check_query, query}, socket) do
+    if socket.assigns.query == query do
+      suggested_songs = get_suggested_songs(query, socket.assigns.spotify_data)
+      {:noreply, assign(socket, :suggested_songs, suggested_songs)}
+    else
+      {:noreply, socket}
+    end
+  end
 
   # =================================
   # Internal Functions
   # =================================
 
   defp apply_action(socket, :edit, %{"id" => song_id}) do
+    song = Session.get_song!(song_id, socket.assigns.session_id)
+
     socket
     |> assign(:page_title, "Edit Song")
-    |> assign(:song, Session.get_song!(song_id, socket.assigns.session_id))
+    |> assign(:song,       song)
+    |> assign(:changeset,  Session.change_song(song))
   end
   defp apply_action(socket, :new, _params) do
+    song = %Song{}
+
     socket
     |> assign(:page_title, "Add Song")
-    |> assign(:song, %Song{})
+    |> assign(:song,       song)
+    |> assign(:changeset,  Session.change_song(song))
   end
   defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing Session")
-    |> assign(:song, nil)
+    |> assign(:song,       nil)
+  end
+
+  defp loading?(""),    do: nil
+  defp loading?(_query), do: :loading
+
+  defp get_suggested_songs("", _spotify_data), do: nil
+  defp get_suggested_songs(query, spotify_data) do
+    case Spotify.search_tracks(query, spotify_data) do
+      {:ok, %Search{tracks: %Tracks{items: tracks}}} when tracks != [] ->
+        convert_tracks(tracks)
+
+      _response ->
+        nil
+    end
+  end
+
+  defp save_song(socket, :new, track_id) do
+    Session.create_song(
+      track_id,
+      socket.assigns.session_id,
+      socket.assigns.spotify_data,
+      socket.assigns.username,
+      socket.assigns.user_id
+    )
+
+    socket
+    |> put_flash(:info, "Song created successfully")
+    |> push_redirect(to: ~p"/session")
+  end
+  defp save_song(socket, :edit, track_id) do
+    Session.update_song(
+      track_id,
+      socket.assigns.session_id,
+      socket.assigns.spotify_data,
+      socket.assigns.song
+    )
+
+    socket
+    |> put_flash(:info, "Song updated successfully")
+    |> push_redirect(to: ~p"/session")
+  end
+
+  defp convert_tracks([]) do
+    []
+  end
+  defp convert_tracks([track = %Track{} | tracks]) do
+    [{"#{track.name} - #{Song.artists(track.artists)}", track.id} | convert_tracks(tracks)]
   end
 end
